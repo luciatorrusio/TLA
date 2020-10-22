5a9e89ca0d5f26dd23fe269d9a70cfc37ef575d3
luciatorrusio <lucia.torrusio@gmail.com>
2020-10-21 22:53:11 -0400

    pil to open cv

--- pruebas_py/prueba1.py
+++ pruebas_py/prueba1.py
@@ -3,7 +3,8 @@
 from load_image import load_image
 from rotate_image import rotate_image
 from show_image import show_image
+from resize_image import resize_image
 
 img = load_image("image.png")
-rotated = rotate_image(img, 45)
+rotated =resize_image(img, 45)
 show_image(rotated)
--- pruebas_py/resize_image.py
+++ pruebas_py/resize_image.py
@@ -1,9 +1,16 @@
 from PIL import Image
+import cv2
+import numpy as np
 def resize_image(image, pixels):
     basewidth = pixels
-    img = Image.open(image)
-    wpercent = (basewidth/float(img.size[0]))
-    hsize = int((float(img.size[1])*float(wpercent)))
-    img = img.resize((basewidth,hsize), Image.ANTIALIAS)
-    img.save(image) 
-    return img
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    im_pil = Image.fromarray(img)
+    wpercent = (basewidth/float(img_pil.size[0]))
+    hsize = int((float(img_pil.size[1])*float(wpercent)))
+    img_pil = img_pil.resize((basewidth,hsize), Image.ANTIALIAS) 
+    numpy_image=numpy.array(img_pil)  
+    opencv_image=cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR)
+    return opencv_image
+
+
+    # https://stackoverflow.com/questions/43232813/convert-opencv-image-format-to-pil-image-format
