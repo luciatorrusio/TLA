4d91ca900fd003202795ed0a5a971f51c854c282
Nicolás <ncomerci@itba.edu.ar>
2020-11-27 22:39:27 -0300

    organized and clean project

--- run_tests.sh
+++ run_tests.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-#fileName = $(ls test | grep '^impDone');
-#echo $(fileName);
-#for name in $a
-#  execv ./target/compiler < name
-#  python translated.py
-#done
-
 for x in test/*.test; do 
   ./target/compiler < $x 2> /dev/null
   echo "Running $x"
   echo ""
-  python3 translated.py
+  python3 out
   echo ""
 done
